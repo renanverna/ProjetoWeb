c0b42e3081a05d068245a6ebb69f3b647245d8f3
RvBSvnhO <RvBSvnhO@RvBSvnhO-PC>
2016-03-29 04:22:15 -0300

    Web version 0.1

--- ProjetoWeb/src/controller/ManterProduto.java
+++ ProjetoWeb/src/controller/ManterProduto.java
@@ -1,18 +1,22 @@
-package Controller;
+package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import classenegocio.Produto;
 import transferobject.ProdutoTO;
 
-
-
-
-@WebServlet("/ManterProduto")
+@WebServlet("/ManterProduto.do")
 public class ManterProduto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
@@ -21,27 +25,32 @@ public class ManterProduto extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		String pAcao = request.getParameter("acao");
 		String pDescricao = request.getParameter("descricao");
-		double pValorCompra = request.getParameter("valor_compra");
-		double pValorVenda = request.getParameter("valor_venda");
+		String pValorCompra = request.getParameter("valor_compra");
+		String pValorVenda = request.getParameter("valor_venda");
 		
-		Produto produto = new Produto(pDescricao, pValorCompra, pValorVenda, 20);
+		double valorCompra = Double.parseDouble(pValorCompra);
+		double valorVenda = Double.parseDouble(pValorVenda);
+		
+		Produto produto = new Produto(pDescricao, valorCompra, valorVenda, 20);
 		
 		if(pAcao.equals("Inserir")){
 			produto.criar();
 		}
 		produto.carregar();
+	
 		
 		ProdutoTO to = new ProdutoTO();
+		System.out.println(produto.getId());
+		
 		to.setId(produto.getId());
 		to.setDescricao(produto.getDescricao());
 		to.setValor_compra(produto.getValor_compra());
 		to.setValor_venda(produto.getValor_venda());
 		
 		RequestDispatcher view = 
-				request.getRequestDispatcher("Cliente.jsp");
+				request.getRequestDispatcher("Produto.jsp");
 	view.forward(request, response);
 	}
 
